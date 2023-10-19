4796266901e43c862d7c50533f42177b38798d78
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-19 16:23:05 +0900

    Add files via upload

---
+++ src/main/java/my/site/project/Kakosecurity4Application.java
@@ -0,0 +1,13 @@
+package com.fullstack2.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kakosecurity4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kakosecurity4Application.class, args);
+	}
+
+}
