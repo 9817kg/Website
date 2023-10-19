29830dc61791a8e74265c255eac90a28f773bc35
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-19 16:18:40 +0900

    Add files via upload

---
+++ src/main/java/my/site/project/dto/MemberDTO.java
@@ -0,0 +1,39 @@
+package com.fullstack2.website.dtos;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String postalCode;
+    private String addressBasic;
+    private String addressRest;
+    private String type;
+    private String phoneArea;
+    private String phoneNumber;
+    private String phoneExt;
+    private String mobileArea;
+    private String mobileNumber;
+    private String mobileExt;
+    private String birthYear; // 수정: 생년
+    private String birthMonth; // 수정: 생월
+    private String birthDay; // 수정: 생일
+    
+    private String role;
+    private String provider;
+    
+}
