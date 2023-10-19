eff02bbc37df857c205dde6a7a01964a3875e45a
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-19 16:43:48 +0900

    Update MemberQuery.java

--- src/main/java/my/site/project/repository/MemberQuery.java
+++ src/main/java/my/site/project/repository/MemberQuery.java
@@ -1,11 +1,11 @@
-package com.fullstack2.website.repository;
+package my.site.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.fullstack2.website.entity.Member;
+import my.site.project.entity.Member;
 
 import jakarta.transaction.Transactional;
 
